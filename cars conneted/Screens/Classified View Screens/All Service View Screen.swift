610785972854f89c5a36f07b9e39b33eb2043152
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2022-12-14 16:31:47 +0500

    login api

--- cars conneted/Screens/Classified View Screens/All Service View Screen.swift
+++ cars conneted/Screens/Classified View Screens/All Service View Screen.swift
@@ -10,7 +10,20 @@ import SwiftUI
 struct All_Service_View_Screen: View {
     @Environment (\.presentationMode) var presentationMode
     @State private var searchText  = ""
+    @State private var addFilter  = false
+    
+    @State var distanceValue : Int = 100
+    
+   
+    
+    @State private var selectType = ""
+    
+    @State private var searchTextFilter  = ""
+    
+    @State private var toSearch  = false
+    
     var body: some View {
+        ZStack{
         VStack{
             
             VStack{
@@ -21,7 +34,7 @@ struct All_Service_View_Screen: View {
                     Button(action: {
                         self.presentationMode.wrappedValue.dismiss()
                     }, label: {
-                        Image("back icon")
+                        Image("BackIconWhite")
                                .resizable()
                                .aspectRatio(contentMode: .fit)
                                .frame(width: 35, height: 35)
@@ -39,7 +52,7 @@ struct All_Service_View_Screen: View {
                         Image("plus icon")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 26, height: 26)
+                            .frame(width: 30, height: 30)
                     })
                   
                     
@@ -56,12 +69,28 @@ struct All_Service_View_Screen: View {
                         .foregroundColor(.red)
                     
                     
+                    NavigationLink(destination: {
+                        All_Service_View_Screen()
+                    }, label: {
                         Image(systemName: "magnifyingglass")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width:20,height: 20)
                             .foregroundColor(.red)
                   
+                    })
+                    
+                    Button(action: {
+                        self.addFilter = true
+                    }, label: {
+                        Image("Filter 2")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                       
+                           
+                    })
+
                    
                     
                     
@@ -75,7 +104,7 @@ struct All_Service_View_Screen: View {
                 
                 
             }
-            .padding(.top,30)
+            .padding(.top,40)
                 .padding(.bottom,15)
                 .background(
                     Image("home screen background")
@@ -98,8 +127,177 @@ struct All_Service_View_Screen: View {
             }
                        
         }
+            
+            HalfASheetFilter(isPresented: $addFilter){
+                
+                VStack{
+                    HStack{
+                        Text("Filter Your Search")
+                        Spacer()
+                    }
+                    .padding(.top)
+                    
+                    HStack{
+                        
+                        TextField("Search",text: self.$searchTextFilter)
+                            .foregroundColor(.red)
+                        
+                        
+                     
+                            Button(action: {
+                                self.addFilter = false
+                                self.toSearch = true
+                            }, label: {
+                                Image(systemName: "magnifyingglass")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width:20,height: 20)
+                                    .foregroundColor(.red)
+                          
+                            })
+                        
+                      
+                           
+                        
+                        
+                    }
+                    .padding(15)
+                    .background(RoundedRectangle(cornerRadius: 10).strokeBorder(.red).frame(width: UIScreen.widthBlockSize*90))
+                    .padding(.top,10)
+                    .padding(.leading,20)
+                    .padding(.trailing,20)
+                    
+                    
+                    
+                    Text("Selext Type")
+                      .padding(.top,20)
+                      .frame(maxWidth: .infinity, alignment: .leading)
+                      
+                      .padding(.bottom,-5)
+                    
+                      
+                    TextField("Body parts, Engine parts, etc",text:$selectType)
+                        .foregroundColor(AppColors.redGradientColor1)
+                      .padding(.vertical, 10)
+                      .autocapitalization(.none)
+                      .background(Rectangle().frame(width:UIScreen.widthBlockSize*90, height: 1).padding(.top, 40))
+                      .foregroundColor(.black)
+                      
+                      .overlay(HStack{
+                        Spacer()
+                       
+                          Menu(content:{
+                              Button(action: {}, label: {
+                                  Text("Body parts")
+                              })
+                              
+                              Button(action: {}, label: {
+                                  Text("Engine parts")
+                              })
+                              
+                          }, label:{
+                              Image("dropdown menu 2")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 20, height: 20)
+                                    .padding()
+                          })
+                          
+                          
+                      })
+                    
+                    HStack{
+                        Text("Price Range")
+                        Spacer()
+                    }
+                    .padding(.top)
+                    
+                    SeekBar(lable: "$", maxRange: 5000, color: AppColors.redGradientColor2, value: self.$distanceValue)
+                        
+                        .padding(.bottom)
+                    
+                    
+                    
+                    HStack{
+                        Text("Rating")
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        
+                        HStack{
+                            Text("1")
+                            
+                            Image("Star")
+                        }
+                        .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.gray.opacity(0.3)))
+                        
+                        HStack{
+                            Text("2")
+                            
+                            Image("Star")
+                        }
+                        .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.gray.opacity(0.3)))
+                        
+                        HStack{
+                            Text("3")
+                            
+                            Image("Star")
+                        }
+                        .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.gray.opacity(0.3)))
+                        
+                        
+                        HStack{
+                            Text("4")
+                            
+                            Image("Star")
+                        }
+                        .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.gray.opacity(0.3)))
+                        
+                        
+                        HStack{
+                            Text("5")
+                            
+                            Image("Star")
+                        }
+                        .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.gray.opacity(0.3)))
+                        
+                        
+                        
+                        
+                        
+                        
+                    }
+                    
+                    
+                    Button(action: {
+                        self.addFilter = false
+                        self.toSearch = true
+                    }, label: {
+                        Text("Apply Filter")
+                            .font(AppFonts.semiBold_16)
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+                            .frame(width: UIScreen.widthBlockSize*90, height: UIScreen.heightBlockSize*7)
+                            .background(RoundedRectangle(cornerRadius: 50).fill(LinearGradient(colors: [AppColors.redGradientColor1, AppColors.redGradientColor2], startPoint: .leading, endPoint: .trailing)))
+                            .padding(.top)
+                            .padding(.bottom)
+                    })
+                     
+                }
+            }
+            .height(.proportional(0.60))
+            .closeButtonColor(UIColor.gray)
+            .backgroundColor(UIColor.white)
+        
+        }
         .edgesIgnoringSafeArea(.all)
-        .navigationBarHidden(true)
+            .navigationBarHidden(true)
     }
 }
 
